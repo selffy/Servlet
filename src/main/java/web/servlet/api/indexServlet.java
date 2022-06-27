0d86aaf512252c10cdce66ea0b8608184a49cea6
selffy <self-uong15@naver.com>
2022-06-27 21:57:31 +0900

    servlet 활용하여 백앤드

---
+++ src/main/java/web/servlet/api/indexServlet.java
@@ -0,0 +1,31 @@
+package web.servlet.api;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/*
+ * 요청의 종류
+ * 1. view(HTML) 요청 -> 무조건 get요청(주소창)
+ * 2. api(Application Programming Interface) 요청 -> CRUD(post, get, put, delete): JavaScript(AJAX 동기, 비동기)
+ * 
+ */
+
+@WebServlet("/index")
+public class indexServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		RequestDispatcher requestDispatcher = request.getRequestDispatcher("/WEB-INF/views/index.jsp");
+		request.getRequestDispatcher("/WEB-INF/views/index.jsp").forward(request, response);
+	}
+
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,30 @@
+<%@ page language="java" contentType="text/html; charset=EUC-KR"
+    pageEncoding="EUC-KR"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="EUC-KR">
+<title>메인페이지</title>
+<link rel="stylesheet" href="/static/css/style.css">
+<link rel="stylesheet" href="/static/css/index.css">
+</head>
+<body>
+
+	<div id = "container">
+		<header>
+			<nav>
+				<ul>
+					<li>회원가입</li>
+					<li>로그인</li>
+					<li>게시판</li>
+				</ul>
+			</nav>
+		</header>
+		<main>
+		</main>
+		<footer>
+		</footer>
+	</div>
+	<script type="text/javascript" src= "/static/js/index.js"></script>
+</body>
+</html>
